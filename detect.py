e5887cc58797106c7b9beef1e9921549b859ffc8
Chengyu Jia <30360640+kenan976431@users.noreply.github.com>
2025-08-08 11:37:58 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -18,7 +18,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 tf.compat.v1.disable_eager_execution()
 
 # %%
-model = load_model('/data0/jinhaibo/lixiaohao/adv_cifar10/Vgg16/VGG16.h5')
+model = load_model('adv_cifar10/Vgg16/VGG16.h5')
 model.summary()
 
 def init_coverage_tables(model1):
@@ -46,30 +46,21 @@ x_train = x_train / 255.0
 x_test = x_test / 255.0
 y_train = to_categorical(y_train, num_classes=10)
 y_test = to_categorical(y_test, num_classes=10)
-# advs = np.load('/home/lxh/lixiaohao/code/dataset/cifar10/VGG19/adv/Square/x_train_adv.npy')
-# x_diff = advs[2] - x_train[2]
-
-# x_diff_1 = x_diff.reshape[-1]
-# plt.imshow(x_diff)
-# plt.show()
-# x_train = np.load('/data0/jinhaibo/DGAN/animals_10_datasets/vgg/train/img_data.npy')
-# x_train = x_train/255.0
-# y_train = np.load('/data0/jinhaibo/DGAN/animals_10_datasets/vgg/train/img_data_label.npy')
 
 # %%
 benign_features = []
 adv_features = []
-# path = '/public/lixiaohao/features4demo/CIFAR10/VGG19/FGSM/Adv'
+# path = 'features4demo/CIFAR10/VGG19/FGSM/Adv'
 for i in range(10):
-    path_Benign = '/data0/jinhaibo/lixiaohao/adv_cifar10/Vgg16/clean/' + str(i) + '.npy'
+    path_Benign = 'dv_cifar10/Vgg16/clean/' + str(i) + '.npy'
     benign_features.extend(np.load(path_Benign))
-    path_adv = '/data0/jinhaibo/lixiaohao/adv_cifar10/Vgg16/FGSM/' + str(i) + '.npy'
+    path_adv = 'adv_cifar10/Vgg16/FGSM/' + str(i) + '.npy'
     adv_features.extend(np.load(path_adv))
 # data = np.array(benign_features)
 # print(data.shape)
 # %%
 test_features = []
-path = '/data0/jinhaibo/lixiaohao/adv_cifar10/Vgg16/JSMA/'
+path = 'adv_cifar10/Vgg16/JSMA/'
 for i in range(10):
     path_adv = path + '/' + str(i) + '.npy'
     test_features.extend(np.load(path_adv))
@@ -101,19 +92,8 @@ y_to_test_one = to_categorical(y_to_test, 2)
 x_to_test_auc = np.vstack((test_features[sec_num:], benign_features[sec_num:]))
 y_to_test_auc = np.vstack((test_labels[sec_num:], benign_labels[sec_num:]))
 y_to_test_auc_one = to_categorical(y_to_test_auc, 2)
+
 #%%
-# import math
-# for i in range(x_to_train.shape[0]):
-#     if i == 0:
-#         x_to_train_hhh = x_to_train[i:i+1]
-#         y_to_train_hhh = y_to_train_one[i:i+1]
-#     else:
-#         if math.isnan(np.max(x_to_train[i:i + 1])):
-#             continue
-#         else:
-#             x_to_train_hhh = np.vstack((x_to_train_hhh, x_to_train[i:i+1]))
-#             y_to_train_hhh = np.vstack((y_to_train_hhh, y_to_train_one[i:i + 1]))
-# %%
 def meta_classify(input_shape):
     # x = Dense(1024, name='predictions', activation='relu')(input_shape)
     x = Dense(2, activation='softmax')(input_shape)
@@ -121,10 +101,9 @@ def meta_classify(input_shape):
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-
 model_detect = meta_classify(input_shape=Input(shape=x_to_train.shape[1:]))
 model_detect.fit(x_to_train_hhh, y_to_train_hhh, batch_size=5, epochs=30)
-# model_detect.save('/home/lxh/lixiaohao/features4demo/CIFAR10/VGG19/cifar_fgsm_vgg19.h5')
+# model_detect.save('features4demo/CIFAR10/VGG19/cifar_fgsm_vgg19.h5')
 # %%ACC score
 loss, acc = model_detect.evaluate(x_to_test, y_to_test_one)
 loss_benign, acc_benign = model_detect.evaluate(benign_features[sec_num:], to_categorical(benign_labels[sec_num:], 2))
@@ -133,3 +112,4 @@ print("Loss: %f; acc: %f; loss_beign: %f; acc_benign: %f" % (loss, acc, loss_ben
 detect_pred_test = model_detect.predict(x_to_test_auc)
 roc_value = roc_auc_score(y_to_test_auc_one, detect_pred_test)
 print('roc_value', roc_value)
+
