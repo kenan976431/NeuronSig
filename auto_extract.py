04df7a15779befcf3dc7e72bdba6dbaa69a9dbb0
Chengyu Jia <30360640+kenan976431@users.noreply.github.com>
2025-08-08 11:34:11 +0800

    Update auto_extract.py

--- auto_extract.py
+++ auto_extract.py
@@ -17,12 +17,6 @@ from art.estimators.classification import KerasClassifier
 import tensorflow as tf
 from keras.utils import to_categorical
 
-# adv = np.load('/public/lixiaohao/code/dataset/cifar10/VGG19/adv/JSMA/train/adv_test_x.npy')
-# print(len(adv))
-# batch_max = len(adv) / 50
-# print(batch_max)
-# noise = [0.01, 0.02, 0.03, 0.05, 0.1, 0.12, 0.13, 0.15, 0.2]
-# for i in noise:
-# os.system('source activate py36_lxh')
 for j in range(43):
     os.system('python feature_extract.py --batch {}'.format(j))
+
